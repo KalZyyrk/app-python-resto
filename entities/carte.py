2728798fd1634f1e4968cd808b58358d6255c344
Ulysse <ulysse.hischke.dev@gmail.com>
2023-09-20 16:10:28 +0200

    create entities for db

---
+++ entities/carte.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Integer, Column, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+from config.database import Base
+
+
+class Carte(Base):
+    __tablename__ = "carte"
+    id_carte = Column(Integer, primary_key=True, autoincrement=True)
+    carte_gastronomique = Column(Boolean, nullable=False)
+    id_restaurant = Column(
+        Integer, ForeignKey("restaurant.id_restaurant"), nullable=False
+    )
+    restaurant = relationship("restaurant")
